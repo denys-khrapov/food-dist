99ec86992a3b6415a7c6a941eb5a3575b324901e
Khrapov Denys <khrapov.denys@gmail.com>
2022-05-27 00:18:15 +0300

    modified script, add rest operator

--- js/script.js
+++ js/script.js
@@ -152,12 +152,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	// Cards Menu
 	class MenuCard {
-		constructor(src, alt, title, descr, price, parentSelector) {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
 			this.src = src;
 			this.alt = alt;
 			this.title = title;
 			this.descr = descr;
 			this.price = price;
+			this.classes = classes;
 			this.parent = document.querySelector(parentSelector);
 			this.transfer = 27;
 			this.changeToUAH();
@@ -169,8 +170,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		render() {
 			const element = document.createElement('div');
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className => element.classList.add(className));
+			}
 			element.innerHTML = `
-			<div class="menu__item">
 				<img src=${this.src} alt=${this.alt}>
 				<h3 class="menu__item-subtitle">${this.title}</h3>
 				<div class="menu__item-descr">${this.descr}</div>
@@ -179,7 +185,6 @@ window.addEventListener('DOMContentLoaded', () => {
 					<div class="menu__item-cost">Цена:</div>
 					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
 				</div>
-			</div>
 			`;
 			this.parent.append(element);
 		}
@@ -191,7 +196,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		`Меню “Фитнес” - это новый подход к приготовлению блюд: больше свежих
 овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
 		9,
-		'.menu .container'
+		'.menu .container',
+		'menu__item'
+
 
 	).render();
 	new MenuCard(
@@ -202,7 +209,8 @@ window.addEventListener('DOMContentLoaded', () => {
 		и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода
 		в ресторан!`,
 		25,
-		'.menu .container'
+		'.menu .container',
+		'menu__item'
 
 	).render();
 	new MenuCard(
@@ -213,7 +221,8 @@ window.addEventListener('DOMContentLoaded', () => {
 		продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное
 		количество белков за счет тофу и импортных вегетарианских стейков.`,
 		15,
-		'.menu .container'
+		'.menu .container',
+		'menu__item'
 
 	).render();
 });
